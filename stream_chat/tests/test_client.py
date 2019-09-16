5ef992774ed20b2cd4f321f3a55bf8a4f2913e65
Peter van Kampen <pterk@datatailors.com>
2019-09-16 22:33:33 +0200

    try to fix heisenbug on travis

--- stream_chat/tests/test_client.py
+++ stream_chat/tests/test_client.py
@@ -133,7 +133,7 @@ class TestClient(object):
         assert [50, 38, 36, 28] == [u["age"] for u in response["users"]]
 
     def test_query_channels_members_in(self, client, fellowship_of_the_ring):
-        response = client.query_channels({"members": {"$in": ["gimli"]}}, {"id": 1})
+        response = client.query_channels({"members": {"$in": ["gimli"]}}) # , {"id": 1})
         assert len(response["channels"]) == 1
         assert response["channels"][0]["channel"]["id"] == "fellowship-of-the-ring"
         assert len(response["channels"][0]["members"]) == 9
