f1a25ab2d9508057a87d145a74c10be767ac7c19
Tommaso Barbugli <tbarbugli@gmail.com>
2019-10-19 11:30:24 +0200

    release 1.0

--- stream_chat/__pkg__.py
+++ stream_chat/__pkg__.py
@@ -1,6 +1,6 @@
 __author__ = "Tommaso Barbugli"
 __copyright__ = "Copyright 2019, Stream.io, Inc"
-__version__ = "0.5.0"
+__version__ = "1.0.0"
 __maintainer__ = "Tommaso Barbugli"
 __email__ = "support@getstream.io"
 __status__ = "Production"
