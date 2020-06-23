0439ac637a92cc38507a998a0a2011cc801f6875
ferhat elmas <elmas.ferhat@gmail.com>
2020-06-23 18:42:56 +0200

    Bump version to v1.3.1

--- stream_chat/__pkg__.py
+++ stream_chat/__pkg__.py
@@ -1,6 +1,6 @@
 __author__ = "Tommaso Barbugli"
 __copyright__ = "Copyright 2019-2020, Stream.io, Inc"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __maintainer__ = "Tommaso Barbugli"
 __email__ = "support@getstream.io"
 __status__ = "Production"
