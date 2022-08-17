683529d6684454434580c8d6768e7dd541bfa86c
ferhat elmas <elmas.ferhat@gmail.com>
2022-08-17 16:00:30 +0200

    fix: handle broken rate limit as best effort (#126)

--- stream_chat/types/stream_response.py
+++ stream_chat/types/stream_response.py
@@ -38,12 +38,21 @@ class StreamResponse(dict):
         )
         if limit and remaining and reset:
             self.__rate_limit = RateLimitInfo(
-                limit=int(limit),
-                remaining=int(remaining),
-                reset=datetime.fromtimestamp(float(reset), timezone.utc),
+                limit=int(self._clean_header(limit)),
+                remaining=int(self._clean_header(remaining)),
+                reset=datetime.fromtimestamp(
+                    float(self._clean_header(reset)), timezone.utc
+                ),
             )
 
         super(StreamResponse, self).__init__(response_dict)
+
+    def _clean_header(self, header: str) -> int:
+        try:
+            values = (v.strip() for v in header.split(","))
+            return int(next(v for v in values if v))
+        except ValueError:
+            return 0
 
     def rate_limit(self) -> Optional[RateLimitInfo]:
         """Returns the ratelimit info of your API operation."""
