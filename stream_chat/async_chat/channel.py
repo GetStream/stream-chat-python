97cb3ee2bc6707d04a63ab2590d2e46f1cb6bf58
Lennart <1247198+totalimmersion@users.noreply.github.com>
2024-12-06 15:21:11 +0100

    fix: update stream_chat/async_chat/channel.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- stream_chat/async_chat/channel.py
+++ stream_chat/async_chat/channel.py
@@ -238,15 +238,13 @@ class Channel(ChannelInterface):
     async def unarchive(self, user_id: str) -> StreamResponse:
         if not user_id:
             raise StreamChannelException("user_id must not be empty")
-        
-        payload = {
-            "set": {
-                "archived": False
-            }
-        }
+
+        payload = {"set": {"archived": False}}
         return self.client.patch(f"{self.url}/member/{user_id}", data=payload)
 
-    async def update_member_partial(self, user_id: str, to_set: Dict = None, to_unset: Iterable[str] = None) -> Union[StreamResponse, Awaitable[StreamResponse]]:
+    async def update_member_partial(
+        self, user_id: str, to_set: Dict = None, to_unset: Iterable[str] = None
+    ) -> Union[StreamResponse, Awaitable[StreamResponse]]:
         if not user_id:
             raise StreamChannelException("user_id must not be empty")
 
